7c0dbf8014d17ccc25761de008ea16229d332cb6
MustafaCelal <m.cllglr@gmail.com>
2023-03-12 02:54:05 +0300

    feat: dealer

---
+++ src/main/java/com/mcll/cms/business/service/DealerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.mcll.cms.business.service;
+
+import com.mcll.cms.data.entity.Dealer;
+import com.mcll.cms.data.repository.DealerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DealerServiceImpl extends BaseServiceImpl<Dealer> implements DealerService {
+
+    public DealerServiceImpl(DealerRepository repository) {
+        super(repository);
+    }
+}
---
+++ src/main/java/com/mcll/cms/data/repository/DealerRepository.java
@@ -0,0 +1,8 @@
+package com.mcll.cms.data.repository;
+
+import com.mcll.cms.data.entity.Dealer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealerRepository extends BaseRepository<Dealer> {
+}
